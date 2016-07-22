f6030712914069418c8709cfd1c521af7630816f
xu.hel <xu.hel@outlook.com>
2016-07-22 11:54:11 +0800

    添加CSS 压缩任务
    Signed-off-by: xu.hel <xu.hel@outlook.com>

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,13 @@ var browserSync=require('browser-sync');
 var gulp=require('gulp');
 var reload=browserSync.reload;
 var less=require('gulp-less');
+var minifycss=require('gulp-minify-css');
 var rename=require('gulp-rename');
 var uglify=require('gulp-uglify');
 var config={
+  less:{all:'./public/styles/**/*.less',src:'./public/styles/*.less',dest:'./build/styles',settings:{}},
   js:{src:'./public/scripts/**/*',dest:'./build/scripts'},
-  less:{all:'./public/styles/**/*.less',src:'./public/styles/*.less',dest:'./build/styles',settings:{}}
+  css:{src:'./public/styles/**/*',dest:'./build/styles'}
 };
 gulp.task('server',function(){
   browserSync({server:{baseDir:'public'}});
@@ -15,6 +17,9 @@ gulp.task('server',function(){
 });
 gulp.task('less',function(){
 	return gulp.src(config.less.src).pipe(less(config.less.settings)).pipe(gulp.dest(config.less.dest))
+});
+gulp.task('css',function(){
+  return gulp.src(config.css.src).pipe(minifycss()).pipe(gulp.dest(config.css.dest));
 });
 gulp.task('uglify',function(){
   return gulp.src(config.js.src)
