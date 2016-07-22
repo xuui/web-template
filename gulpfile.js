65cd23a1b236792eb440ada69cc0aab5ed5f1e82
xu.hel <xu.hel@outlook.com>
2016-07-22 11:54:11 +0800

    添加 gulp-uglify 用来压缩 js 文件。
    Signed-off-by: xu.hel <xu.hel@outlook.com>

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,19 @@
 var browserSync=require('browser-sync');
 var gulp=require('gulp');
 var reload=browserSync.reload;
+var rename=require('gulp-rename');
+var uglify=require('gulp-uglify');
+var config={
+  js:{src:'./public/scripts/**/*',dest:'./build/scripts'},
+};
 gulp.task('server',function(){
   browserSync({server:{baseDir:'public'}});
   gulp.watch(['*.html','styles/**/*.css','scripts/**/*.js'],{cwd:'public'},reload);
+});
+gulp.task('uglify',function(){
+  return gulp.src(config.js.src)
+  .pipe(gulp.dest(config.js.dest))
+  .pipe(uglify())
+  .pipe(rename({extname:'.min.js'}))
+  .pipe(gulp.dest(config.js.dest));
 });
